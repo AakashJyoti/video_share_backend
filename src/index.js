d347111571d71bf7c9242ffde90f1ff8c62f4071
AakashJyoti <akjyoti572@gmail.com>
2023-11-23 23:57:55 +0530

    dot env added different

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-// require('dotenv').config({path: "../.env"})
-
+import "dotenv/config.js";
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
-import dotenv from "dotenv";
-
-dotenv.config({
-  path: "./env",
-});
 
 const PORT = process.env.PORT || 8000;
 
